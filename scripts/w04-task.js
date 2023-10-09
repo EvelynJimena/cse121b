3a37ab8734d347dc233f0e0d262eace535b64cdf
EvalynJimena <evelynjimenaez@gmail.com>
2023-10-09 11:18:27 -0500

    done

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -2,15 +2,74 @@
 
 /* Profile Object  */
 
+const myProfile = {
+    name: 'Evelyn Espinoza',
+    photo: 'images/EvelynJimena.jpg',
 
-
-
+    favoriteFoods: [
+    'Lasaña',
+    'Pizza',
+    'Camarones apanados',
+    'Pasta',
+],
+     hobbies: [
+    'Paint Nails',
+    'Play videogames',
+    'Go to the beach',
+],
+    placesLived : []
+};
 /* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: 'Quevedo, Ec',
 
+        length: ' 15 years ',
+    },
 
+    {
+        place : 'San Clemente, Ec',
+
+        length : ' 4 years ',
+    },
+    {
+        place : 'Portoviejo , Ec',
+
+        length : '4 years'
+    }
+)
 
 
 /* DOM Manipulation - Output */
+document.querySelector('#name').textContent= myProfile.name;
+const photo = document.querySelector('#photo');
+photo.setAttribute('src',myProfile.photo);
+photo.setAttribute('alt',myProfile.name);
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li)
+})
+myProfile.hobbies.forEach(hobbie => {
+    let li = document.createElement('li');
+    li.textContent = hobbie;
+    document.querySelector('#hobbies').appendChild(li);
+})
+ myProfile.placesLived.forEach(placeLived => {
+    let dt = document.createElement('place');
+    dt.textContent = placeLived.place;
+    let dd = document.createElement('length');
+    dd.textContent = placeLived.length;
+    let dl = document.createElement('dl');
+    dl.textContent = placeLived.length
+    document.querySelector('#places-lived').appendChild(dt);
+    document.querySelector('#places-lived').appendChild(dl);
+    
+    
+    
+})
+
+
 
 /* Name */
 
@@ -24,5 +83,4 @@
 
 
 /* Places Lived DataList */
-
 
